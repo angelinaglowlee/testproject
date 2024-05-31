96de878a13441f9d74cd52544a26a5636d858f39
user <fdsgdfg@gmail.com>
2024-05-31 09:34:14 +0300

    j

---
+++ ConsoleApp4/Trash.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp4;
+
+public class Trash
+{
+    private IList<object> _object = new List<object>();
+    
+    public void AddObjectToTrash(object item)
+    {
+        _object.Add(item);
+    }
+
+    public void ClearTrash()
+    {
+        _object = new List<object>();
+    }
+}
