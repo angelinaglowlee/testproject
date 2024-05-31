78e82d1b618b1ea661a6509dde6f60f5efcd23ef
user <fdsgdfg@gmail.com>
2024-05-31 09:33:04 +0300

    nmhvnh

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -1,3 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using ConsoleApp4;
 Console.WriteLine("Hello, World!");
+
+Trash trash = new Trash();
+
+
+trash.AddObjectToTrash(item:"фунтик");
+trash.AddObjectToTrash(item:"фантик");
+trash.ClearTrash();
+trash.AddObjectToTrash(item:"");
